2e8fcd2a0463b2a5d8115b286a8879474a70df0c
IGOR <igralex1@gmail.com>
2019-03-20 04:24:53 +0300

    comlete: Task warmup is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+// Формула перевода величин из градусов 
+// Фаренгейта -> Цельсий
+// F - фаренгей 
+// С - цельсий
+//
+// C = (F-32)/1.8
+// F = (C * 2) + 32
+
+// The formula for converting values from degrees
+// Fahrenheit -> Celsius
+// F - Fahrenhey
+// C - Celsius
+//
+// C = (F-32) / 1.8
+// F = (C * 2) + 32
+
 module.exports = function warmup(temperature) {
   // your implementation here
+  let celsius = temperature;
+  fahrenheit = ((Math.round(temperature) * 1.8) + 32);
+  console.log(fahrenheit);
+  return fahrenheit;
 };
